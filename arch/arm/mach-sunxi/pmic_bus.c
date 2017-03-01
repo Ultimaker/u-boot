ab4d5e1546325a108e637b355866d5fa5a77306a
Olliver Schinagl <oliver@schinagl.nl>
2017-03-01 12:52:48 +0100

    sunxi: pmic_bus: Decrease boot time by not writing duplicate data
    When we set or clear a pmic_bus bit, we do a read-modify-write
    operation. We waste some time however, by writing back the exact same
    value that was already set in the chip. Let us thus only configure the
    chip if the data is different.
    
    Signed-off-by: Olliver Schinagl <oliver@schinagl.nl>

--- arch/arm/mach-sunxi/pmic_bus.c
+++ arch/arm/mach-sunxi/pmic_bus.c
@@ -95,6 +95,9 @@ int pmic_bus_setbits(u8 reg, u8 bits)
 	if (ret)
 		return ret;
 
+	if (val & bits)
+		return 0;
+
 	val |= bits;
 	return pmic_bus_write(reg, val);
 }
@@ -107,6 +110,9 @@ int pmic_bus_clrbits(u8 reg, u8 bits)
 	ret = pmic_bus_read(reg, &val);
 	if (ret)
 		return ret;
+
+	if (!(val & bits))
+		return 0;
 
 	val &= ~bits;
 	return pmic_bus_write(reg, val);
